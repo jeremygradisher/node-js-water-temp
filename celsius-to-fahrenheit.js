f6b3fc2053bb407bb44d6a76fa1a99382668656e
Ubuntu <ubuntu@ip-172-31-41-8.ec2.internal>
2023-02-24 20:24:41 +0000

    Initial commit

---
+++ celsius-to-fahrenheit.js
@@ -0,0 +1,8 @@
+/* celsius-to-fahrenheit.js */
+/* node celsius-to-fahrenheit.js 100 */
+const { celsiusToFahrenheit } = require('./converters.js');
+ 
+const celsiusInput = process.argv[2]; 
+const fahrenheitValue = celsiusToFahrenheit(celsiusInput);
+ 
+console.log(`${celsiusInput} degrees Celsius = ${fahrenheitValue} degrees Fahrenheit`);
